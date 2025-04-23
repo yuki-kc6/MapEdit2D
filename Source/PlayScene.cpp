31a3db56a1d8aabf29156765a50e48299e77f85e
侑希 今田 <240264@jc-21.jp>
2025-04-23 14:19:03 +0900

    プロジェクト ファイルを追加します。

---
+++ Source/PlayScene.cpp
@@ -0,0 +1,23 @@
+#include "PlayScene.h"
+#include <DxLib.h>
+
+PlayScene::PlayScene()
+{
+}
+
+PlayScene::~PlayScene()
+{
+}
+
+void PlayScene::Update()
+{
+	if (CheckHitKey(KEY_INPUT_T)) {
+		SceneManager::ChangeScene("TITLE");
+	}
+}
+
+void PlayScene::Draw()
+{
+	DrawString(0, 0, "PLAY SCENE", GetColor(255, 255, 255));
+	DrawString(100, 400, "Push [T]Key To Title", GetColor(255, 255, 255));
+}
